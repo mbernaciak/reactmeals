eef3deecdd26412358b8924c8c9f27f09e407730
mbernaciak <m.bernaciak.90@gmail.com>
2023-09-04 22:24:21 +0200

    last changes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -25,7 +25,7 @@ const HeaderCartButton = (props) => {
     return () => {
       clearTimeout(timer);
     };
-  }, [items]);
+  }, [cartCtx.items]);
 
   return (
     <button className={btnClasses} onClick={props.onClick}>
